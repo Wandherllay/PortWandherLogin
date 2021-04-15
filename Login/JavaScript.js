2f0b8f961bd3122b9ccbc8056773f4077ad135f5
WandherllayBatista <wandherllay.13@gmail.com>
2021-04-14 21:24:06 -0300

    Test to Login

--- Login/JavaScript.js
+++ Login/JavaScript.js
@@ -3,7 +3,8 @@ function logar(){
     var chavedeAcesso = document.getElementById("chavedeAcesso").value;
     var senha = document.getElementById("senha").value;
    
-   if(chavedeAcesso == "teste1" && senha == "12345"){
+   
+    if(chavedeAcesso == "teste1" && senha == "12345"){
         window.location.href="http://www.devmedia.com.br/";
         // localStorage.setItem("acesso", true);
         }else{
