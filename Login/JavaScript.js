967e9afb8a77e51edc64d0e2bd88a5b3d55702a7
WandherllayBatista <75100242+Wandherllay@users.noreply.github.com>
2021-08-05 14:58:09 -0300

    Add files via upload

---
+++ Login/JavaScript.js
@@ -0,0 +1,13 @@
+function logar(){
+
+    var chavedeAcesso = document.getElementById("chavedeAcesso").value;
+    var senha = document.getElementById("senha").value;
+   
+   
+    if(chavedeAcesso == "teste1" && senha == "12345"){
+        window.location.href="http://www.devmedia.com.br/";
+        // localStorage.setItem("acesso", true);
+        }else{
+        alert("Chave de Acesso ou Senha invalidos!");
+    }
+}
